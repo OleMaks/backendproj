717e15d7c13dcb9e94525f7cbec4b4394de7c127
AmigoMaks <prokopchuk.maksym@chnu.edu.ua>
2026-05-19 23:20:11 +0300

    a

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -6,26 +6,26 @@ const reportController = require('../controllers/report.controller');
 /**
  * @swagger
  * /api/reports:
- * get:
- * summary: Отримати фінансовий звіт за певний період
- * parameters:
- * - in: query
- * name: from
- * schema:
- * type: string
- * required: true
- * description: Початкова дата (2026-01-01)
- * - in: query
- * name: to
- * schema:
- * type: string
- * required: true
- * description: Кінцева дата (2026-05-31)
- * responses:
- * '200':
- * description: Успішно згенеровано звіт.
- * '400':
- * description: Відсутні обовʼязкові параметри дат.
+ *   get:
+ *     summary: Отримати фінансовий звіт за певний період
+ *     parameters:
+ *       - in: query
+ *         name: from
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Початкова дата (2026-01-01)
+ *       - in: query
+ *         name: to
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Кінцева дата (2026-05-31)
+ *     responses:
+ *       '200':
+ *         description: Успішно згенеровано звіт.
+ *       '400':
+ *         description: Відсутні обовʼязкові параметри дат.
  */
 router.get('/', reportController.getPeriodReport);
 
