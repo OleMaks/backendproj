1cbc2aa75c5fad7d729fd5a36c7a49bdbecea1c9
AmigoMaks <prokopchuk.maksym@chnu.edu.ua>
2026-05-23 22:17:48 +0300

    zxc

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config(); // Цей рядок дозволяє Node.js читати файл .env
 
-// Create a connection pool to the database
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'pmpm2424',
-  database: 'finance_system',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = pool;
